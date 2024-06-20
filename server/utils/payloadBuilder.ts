a9a09cbf5efe4468d8f21c1dce9df3f600ef960c
Bryan Olandres <ozoneblacklight@outlook.com>
2024-06-20 10:58:50 +0800

    properly types payload parser

--- server/utils/payloadBuilder.ts
+++ server/utils/payloadBuilder.ts
@@ -4,13 +4,12 @@
  */
 export default function (dataParameters: string) {
 	const payloadArray = dataParameters.split("\n");
+	const payload: { [key: string]: string } = {};
 
-	const payload = {};
-
-	payloadArray.forEach((item: string) => {
+	for (const item of payloadArray) {
 		const itemArr = item.split("=");
 		payload[itemArr[0]] = itemArr[1];
-	});
+	}
 
 	return payload;
 }
