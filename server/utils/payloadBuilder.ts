39522d5807b83d3d07a81bea3a033172ea5b7c4d
Bryan Olandres <ozoneblacklight@outlook.com>
2024-07-15 14:54:43 +0800

    fixed bug preventing the submission of full URLs with params

--- server/utils/payloadBuilder.ts
+++ server/utils/payloadBuilder.ts
@@ -7,7 +7,9 @@ export default function (dataParameters: string) {
   const payload: { [key: string]: string } = {}
 
   for (const item of payloadArray) {
-    const itemArr = item.split('=')
+    // split only on the first instance of the "=" sign
+    // i mean sure, i could've just indexed the location of the first "=" but where's the fun in that?
+    const itemArr = item.split(/=(.+)/)
     payload[itemArr[0]] = itemArr[1]
   }
 
