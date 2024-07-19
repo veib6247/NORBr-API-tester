068e8289ac96f747f8cfadb13981f8266ee207d4
Bryan Olandres <ozoneblacklight@outlook.com>
2024-07-19 11:37:08 +0800

    update nav

--- app.vue
+++ app.vue
@@ -1,43 +1,8 @@
 <template>
   <div class="flex h-dvh">
     <!-- nav -->
-    <div class="flex h-dvh w-72 flex-col bg-purple-950">
-      <!-- general nav -->
-      <nav class="flex flex-col gap-1 p-6">
-        <h1 class="text-xs font-semibold uppercase text-white opacity-60">
-          App
-        </h1>
-        <NavLink route-to="/"><IconHome /> Home</NavLink>
-      </nav>
-
-      <!-- nav for NORBr -->
-      <nav class="flex flex-col gap-1 p-6">
-        <h1 class="text-xs font-semibold uppercase text-white opacity-60">
-          NORBr
-        </h1>
-        <NavLink route-to="/NORBr/checkout"><IconCart />Checkout</NavLink>
-        <NavLink route-to="/NORBr/hostedelements">
-          <IconShoppingbag />Hosted Elements
-        </NavLink>
-        <NavLink route-to="/NORBr/order"> <IconCash />Order </NavLink>
-        <NavLink route-to="/NORBr/maintenance">
-          <IconReload />Maintenance
-        </NavLink>
-        <NavLink route-to="/NORBr/redirect?status=accept">
-          <IconRedirect />Redirect
-        </NavLink>
-      </nav>
-
-      <!-- nav for Nethone -->
-      <nav class="flex flex-col gap-1 p-6">
-        <h1 class="text-xs font-semibold uppercase text-white opacity-60">
-          Nethone
-        </h1>
-        <NavLink route-to="/Nethone/inquiry"><IconInfo />Inquiry</NavLink>
-        <NavLink route-to="/Nethone/transaction">
-          <IconCard />Transaction
-        </NavLink>
-      </nav>
+    <div class="flex h-dvh w-72 flex-col overflow-auto bg-purple-900">
+      <NavVertical />
     </div>
 
     <!-- content -->
---
+++ components/Nav/Vertical.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="flex flex-col gap-8 p-6">
+    <div class="space-y-2">
+      <h1 class="text-xs font-semibold uppercase text-white opacity-60">App</h1>
+      <UVerticalNavigation
+        :links="generalLinks"
+        :ui="{
+          inactive: 'text-white/80',
+        }"
+      />
+    </div>
+
+    <div class="space-y-2">
+      <h1 class="text-xs font-semibold uppercase text-white opacity-60">
+        NORBr
+      </h1>
+      <UVerticalNavigation
+        :links="norbrLinks"
+        :ui="{
+          inactive: 'text-white/80',
+        }"
+      />
+    </div>
+
+    <div class="space-y-2">
+      <h1 class="text-xs font-semibold uppercase text-white opacity-60">
+        Nethone
+      </h1>
+      <UVerticalNavigation
+        :links="nethoneLinks"
+        :ui="{
+          inactive: 'text-white/80',
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const generalLinks = [
+    {
+      label: 'Home',
+      icon: 'i-heroicons-home',
+      to: '/',
+    },
+  ]
+
+  const norbrLinks = [
+    {
+      label: 'Checkout',
+      icon: 'i-heroicons-shopping-cart',
+      to: '/NORBr/checkout',
+    },
+    {
+      label: 'Hosted Elements',
+      icon: 'i-heroicons-document',
+      to: '/NORBr/hostedelements',
+    },
+    {
+      label: 'Order',
+      icon: 'i-heroicons-banknotes',
+      to: '/NORBr/order',
+    },
+    {
+      label: 'Maintenance',
+      icon: 'i-heroicons-code-bracket-square',
+      to: '/NORBr/maintenance',
+    },
+    {
+      label: 'Redirect',
+      icon: 'i-heroicons-arrow-uturn-left',
+      to: '/NORBr/redirect?status=accept',
+    },
+  ]
+
+  const nethoneLinks = [
+    {
+      label: 'Inquiry',
+      icon: 'i-heroicons-information-circle',
+      to: '/Nethone/inquiry',
+    },
+    {
+      label: 'Transaction',
+      icon: 'i-heroicons-credit-card',
+      to: '/Nethone/transaction',
+    },
+  ]
+</script>
