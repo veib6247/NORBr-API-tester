1a197ca89664835880b42cb426d3e82692111a0c
Bryan Olandres <ozoneblacklight@outlook.com>
2024-08-02 14:23:31 +0800

    dummy page

--- pages/Seon/fraud-api.vue
+++ pages/Seon/fraud-api.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="container mx-auto flex flex-col gap-4">
     <AppPageTitle> Fraud API </AppPageTitle>
+    <p class="text-sm">Under construction</p>
 
     <p class="text-sm">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt,
       obcaecati consequatur at atque corrupti dolor recusandae voluptas aut
       iusto nostrum sunt, alias et inventore, accusamus ullam eos tenetur ipsum
-      totam.
+      totam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta
+      ipsum fuga repellat velit harum, pariatur dolor vitae corrupti quis nam ex
+      earum nisi incidunt qui, unde, omnis nesciunt similique facere.
     </p>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  useUpdateTitle('Fraud API')
+</script>
 
 <style></style>
