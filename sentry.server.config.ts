7a2945ddd46db5e705e4a36496aca7e256e0b772
Bryan Olandres <ozoneblacklight@outlook.com>
2025-06-18 13:29:40 +0800

    added Sentry for error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/nuxt'
+
+Sentry.init({
+  dsn: 'https://3e559a64bb4346466600ca4222ab2f04@o4509517228539904.ingest.de.sentry.io/4509518012547152',
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
